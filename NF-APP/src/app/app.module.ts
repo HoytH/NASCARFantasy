d9838f0cc9799e6cacc98441ca117a8995844ca7
Hoyt Hicks <hoytdawson@gmail.com>
2023-04-08 22:36:11 -0400

    added this login page https://www.youtube.com/watch?v=BHU505buik4

--- NF-APP/src/app/app.module.ts
+++ NF-APP/src/app/app.module.ts
@@ -3,25 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms'
 import { HomeComponent } from './components/home/home.component';
 import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import {MatTabsModule} from '@angular/material/tabs'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
+    LoginComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    BrowserAnimationsModule,
     FontAwesomeModule,
     MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
+    MatTabsModule,
     MatToolbarModule
   ],
   providers: [],
